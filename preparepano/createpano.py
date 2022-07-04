2ab074d1264ec51c63e8574c8c94b145b2c813a1
Werner Bailer <werner.bailer@joanneum.at>
2022-07-04 15:55:33 +0200

    Fix to avoid blending of different class colors
    Applied to colors having values in disjoint channels

--- preparepano/createpano.py
+++ preparepano/createpano.py
@@ -110,7 +110,7 @@ def combine_views(
                 pano = pano + sphere_img 
 
             else:
-                pano[sphere_img>0] = sphere_img[sphere_img>0] 
+                pano[np.any(sphere_img>0,axis=2)] = sphere_img[np.any(sphere_img>0,axis=2)] 
         pano_w[:,:,0] = pano_w[:,:,0] + validMap
         if nchannels>1:
             pano_w[:,:,1] = pano_w[:,:,1] + validMap
